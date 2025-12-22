efb9bec0d63139bf674890e218e2ef7c7d66d3ad
김성제의 Macbook <ksj@gimseongje-uiui-MacBookAir.local>
2025-12-22 13:29:46 +0900

    Change the code to work

--- vector.py
+++ vector.py
@@ -13,18 +13,18 @@ def format_documents(documents):
     return "\n\n".join(d.page_content for d in documents)
 
 
-def load_documents_from_docx(file_path):    
+def load_documents_from_docx(file_path):
     loader = Docx2txtLoader(file_path)
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=200)
 
     return loader.load_and_split(text_splitter)
 
 
-# documents = load_documents_from_docx("/Users/ksj/MyProjects/llm/inflearn-streamlit/tax_with_markdown.docx")
+documents = load_documents_from_docx("~/tax_with_markdown.docx")
 
-# print(f"Loaded {len(documents)} documents from the DOCX file.")
+print(f"Loaded {len(documents)} documents from the DOCX file.")
 
-# database = VectorStoreFactory(os.environ.get("PINECONE_API_KEY")).get_instance()
-# database.add_documents(documents)
+database = VectorStoreFactory(os.environ.get("PINECONE_API_KEY")).get_instance()
+database.add_documents(documents)
 
-# print("Vector store created and documents added.")
+print("Vector store created and documents added.")
